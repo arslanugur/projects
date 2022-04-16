1d924b0427d922b1778ac085d2332b2c519cd3bd
Arslan <45125597+arslanugur@users.noreply.github.com>
2022-04-16 23:59:58 +0300

    Create 05. Add Event Handlers.js

---
+++ Web Programming/Convert Website from Light to Dark Mode/05. Add Event Handlers.js
@@ -0,0 +1,14 @@
+function switchTheme(e) {
+  if (e.terget.checked) {
+    document.documentElement.setAttribute('data-theme', 'dark');
+    localStorage.setItem('theme', 'dark');
+  }
+  else {
+    document.documentElement.setAttribute('data-theme', 'light');
+    localStorage.setItem('theme', 'light');
+  }
+}
+
+toggleSwitch.addEventListener('change', switchTheme, false);
+
+
