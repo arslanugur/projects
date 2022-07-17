d0bf04c2bb1940e86ae5eb1a1f71362004e92b24
Arslan <45125597+arslanugur@users.noreply.github.com>
2022-07-18 00:16:11 +0300

    Update main.py

--- Zoom Clone in Py/main.py
+++ Zoom Clone in Py/main.py
@@ -7,7 +7,7 @@ from vidstream import *
 import tkinter as tk
 import socket
 import threading
-import requests
+# import requests
 
 # To find IP address
 # cmd --> ipconfig --> IPv4 Address
@@ -16,7 +16,36 @@ local_ip_address = socket.gethostbyname(socket.gethostname())
 # server = StreamingServer('192.168.0.207', 9999)
 # print(local_ip_address)
 
+server = StreamingServer(local_ip_address, 9999)
+receiver = AudioReceiver(local_ip_address, 8888)
 
+# PART III
+# Streaming Functions
+
+def start_listening():
+  t1 = threading.Thread(target=server.start_server)
+  t2 = threading.Thread(target=receiver.start_server)
+  t1.start()
+  t2.start()
+
+def start_camera_stream():
+  camera_client = CameraClient(text_target_ip.get(1.0, 'end-1c'), 7777)
+  t3 = threading.Thread(target=camera_client.start_stream)
+  t3.start()
+  
+def start_screen_sharing():
+  screen_client = ScreeenShareClient(text_target_ip.get(1.0, 'end-1c'), 7777)
+  t4 = threading.Thread(target=screen_client.start_stream)
+  t4.start()
+
+def start_audio_stream():
+  audio_sender = AudioSender(text_target_ip.get(1.0, 'end-1c'), 6666) # gonna connect 6666
+  t5 = threading.Thread(target=audio_sender.start_stream)
+  t5.start()  
+  
+  
+  
+  
 # PART II
 # GUI
 window.title("I call v0.0.1 Alpha")
@@ -28,29 +57,24 @@ label_target_ip.pack()
 text_target_ip = tk.Text(window, height=1)
 text_target_ip.pack()
 
-btn_listen = tk.Button(window, text="Start Listening", width=50)
+btn_listen = tk.Button(window, text="Start Listening", width=50, command=start_listening)
 btn_listen.pack(anchor=tk.CENTER, expand=True)
 
-btn_camera = tk.Button(window, text="Start Camera Stream", width=50)
+btn_camera = tk.Button(window, text="Start Camera Stream", width=50, command=start_camera_stream)
 btn_camera.pack(anchor=tk.CENTER, expand=True)
 
-btn_screen = tk.Button(window, text="Start Screen Sharing", width=50)
+btn_screen = tk.Button(window, text="Start Screen Sharing", width=50, command=start_screen_sharing)
 btn_screen.pack(anchor=tk.CENTER, expand=True)
 
-btn_audio = tk.Button(window, text="Start Audio Stream", width=50)
+btn_audio = tk.Button(window, text="Start Audio Stream", width=50, command=start_audio_stream)
 btn_audio.pack(anchor=tk.CENTER, expand=True)
 
 window.mainloop()
 
 
-# PART III
-# Streaming Funtions
-
-
-
-
-
-
+# PART IV
+# Demonstration
+# Connection between two IP
 
 
 
